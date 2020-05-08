06c3c843ea7fafedbd7e270b716546260216b7cb
aali <hello@ahmadali.me>
2020-05-09 02:09:41 +0300

    update

--- frontend/src/_services/real-time.js
+++ frontend/src/_services/real-time.js
@@ -4,8 +4,7 @@ const io = require('socket.io-client');
 class RealTime{
     constructor(){
         this.socket = io.connect("https://fadfed.me/socket.io", {
-            reconnection: true,
-            path: '/notification'
+            reconnection: true
         });
     }
 }
